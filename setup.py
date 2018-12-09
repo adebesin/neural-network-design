a0432bc154cb06d8dbed0deed5378d49901493be
Adebesin <ldev515@pc58585al.retail2u.trcg.co.uk>
2018-12-09 14:02:39 +0000

    notes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='neural-network-design',
+    version='0.1',
+    packages=['src'],
+    url='',
+    license='Apache',
+    author='adebesin',
+    author_email='',
+    description=''
+)
